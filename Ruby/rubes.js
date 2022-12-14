f296383a9d5c0efc920311307a92d7b991c20a1b
RXTArtemis <119773080+RXTArtemis@users.noreply.github.com>
2022-12-13 21:35:06 -0600

    greet the room

--- Ruby/rubes.js
+++ Ruby/rubes.js
@@ -1,1 +1,18 @@
-console.log(Hello everyone! Looking forward to working with everyone!);
+console.log("Hello everyone! Looking forward to working with everyone!");
+let numberOfPeople= prompt("How many people are in the room?");
+
+    console.log("There are 5 people.");
+
+
+let isMorning= prompt("Is it morning?");
+
+let message= "";
+for (let i=0; i<numberOfPeople; i++){
+    if (isMorning==="yes"){
+        ("Good Morning!\n");
+    }
+    else {
+        ("Good afternoon!\n")
+    }
+    alert("Have a great rest of the day")
+} 
