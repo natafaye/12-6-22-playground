02a3ad12e43a3d4ac292f1fd84a4de83c9d2f870
Shari McRae <sharimcrae@protonmail.com>
2022-12-13 21:34:11 -0600

    Added Greet the Room Practice

--- shari-mcrae/scripts.js
+++ shari-mcrae/scripts.js
@@ -1,1 +1,16 @@
 console.log("Inside Shari's javascript file!");
+let numberOfPeople = prompt("How many people are in the room?");
+console.log("numberOfPeople = " + numberOfPeople);
+
+let isMorning = prompt("Is it morning?");
+console.log("isMorning = " + isMorning);
+
+let message = "";
+
+for (let i = 0; i < numberOfPeople; i++) {
+    if (isMorning === "yes")
+        message += "Good morning!\n";
+    else
+        message += "Good afternoon!\n";
+}
+alert(message);
