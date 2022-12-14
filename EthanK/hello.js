6e7fce3ad0580cf2da32e092b08cd45b1ce7a14a
EthanK2 <ethan.karsjen@gmail.com>
2022-12-13 21:34:57 -0600

    ed   EthanK/hello.js

---
+++ EthanK/hello.js
@@ -0,0 +1,17 @@
+let numberOfPeople = prompt('How many people are in the room?');
+
+console.log(numberOfPeople);
+
+let isMorning = prompt('Is it morning?');
+
+let message = "";
+
+for (let i = 0; i < numberOfPeople; i++) {
+    if (isMorning === 'yes') {
+        message += "good Morning!\n"
+    }
+    else {
+    }
+    alert('Hello there, user!')
+
+}
